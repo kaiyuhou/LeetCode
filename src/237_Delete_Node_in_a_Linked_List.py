3d885ee924fdf5b0def9f9a81ff49f4eda8d0696
Mike <mikewell@live.cn>
2015-09-25 17:40:41 +0800

    a test for commit

---
+++ src/237_Delete_Node_in_a_Linked_List.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 17:13:36 2015
+
+@author: Mike
+"""
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+        
+    def disp(self):
+        print self.val,
+        print '->',
+        if self.next :
+            self.next.disp()
+        else:
+            print 
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        dumb = ListNode(-1)        
+        dumb.next = node        
+        p = dumb
+        q = dumb.next
+        
+        while q.next:
+            p = p.next
+            q = q.next
+            p.val = q.val
+            print 'a'
+        p.next.disp()
+        del(p.next)
+        
+testcase = ListNode(0)
+p = testcase
+l = [1]
+for i in l :   
+    q = ListNode(i)
+    p.next = q
+    p = p.next
+testcase.disp()
+
+sln = Solution()
+
+sln.deleteNode(testcase.next)
+testcase.disp()    
